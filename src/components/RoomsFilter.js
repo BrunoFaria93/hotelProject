29d842dfa1ef13c72f564596e80aff059da097b0
Bruno Soares <soares_de_faria@hotmail.com>
2023-09-22 15:19:25 -0300

    feat: new filters of rooms (guests, price, extra)

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -35,6 +35,14 @@ export default function RoomsFilter({ rooms }) {
       </option>
     );
   });
+  let people = getUnique(rooms, "capacity");
+  people = people.map((item, index) => {
+    return (
+      <option key={index} value={item}>
+        {item}
+      </option>
+    );
+  });
   return (
     <>
       <section className="filter-container">
@@ -54,6 +62,59 @@ export default function RoomsFilter({ rooms }) {
             </select>
           </div>
           {/* end select type */}
+          {/* guests */}
+          <div className="form-group">
+            <label htmlFor="capacity">Hóspedes</label>
+            <select
+              name="capacity"
+              id="capacity"
+              value={capacity}
+              className="form-control"
+              onChange={handleChange}
+            >
+              {people}
+            </select>
+          </div>
+          {/* end guests */}
+          {/* room price */}
+          <div className="form-group">
+            <label htmlFor="price">Preço R${price},00</label>
+            <input
+              type="range"
+              name="price"
+              min={minPrice}
+              max={maxPrice}
+              id="price"
+              value={price}
+              onChange={handleChange}
+              className="form-control"
+            />
+          </div>
+          {/* end room price */}
+          {/* extras */}
+          <div className="form-group">
+            <div className="single-extra">
+              <input
+                type="checkbox"
+                name="breakfast"
+                id="breakfast"
+                checked={breakfast}
+                onChange={handleChange}
+              />
+              <label htmlFor="breakfast">breakfast</label>
+            </div>
+            <div className="single-extra">
+              <input
+                type="checkbox"
+                name="pets"
+                id="pets"
+                checked={pets}
+                onChange={handleChange}
+              />
+              <label htmlFor="pets">pets</label>
+            </div>
+          </div>
+          {/* end extra */}
         </form>
       </section>
     </>
--- src/context.js
+++ src/context.js
@@ -54,7 +54,7 @@ class RoomProvider extends Component {
 
   handleChange = (event) => {
     const target = event.target;
-    const value = event.type === "checkbox" ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
     this.setState(
       {
@@ -67,12 +67,36 @@ class RoomProvider extends Component {
   filterRooms = () => {
     let { rooms, type, capacity, price, minSize, maxSize, breakfast, pets } =
       this.state;
+    // all the rooms
     let tempRooms = [...rooms];
+    // transform value
+    capacity = parseInt(capacity);
+    price = parseInt(price);
+    // filter by type
     if (type !== "all") {
       tempRooms = tempRooms.filter((room) => room.type === type);
     }
+
+    // filter by capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter((room) => room.capacity >= capacity);
+    }
+
+    // filter by price
+    tempRooms = tempRooms.filter((room) => room.price <= price);
+
+    // filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter((room) => room.breakfast === true);
+    }
+    // filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter((room) => room.pets === true);
+    }
+
+    // change state
     this.setState({
-      sortedRooms: tempRooms, 
+      sortedRooms: tempRooms,
     });
   };
 
