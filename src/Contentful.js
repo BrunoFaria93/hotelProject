546321724d0d2d65556d76f4a6bbb008faf26e7b
Bruno Soares <soares_de_faria@hotmail.com>
2023-09-24 02:58:02 -0300

    fix: trying to fix the blank page on vercel

--- src/Contentful.js
+++ src/Contentful.js
@@ -3,7 +3,6 @@ import {createClient} from "contentful"
 const cSpace = process.env.REACT_APP_CONTENTFUL_SPACE;
 const cToken = process.env.REACT_APP_CONTENTFUL_TOKEN;
 
-
 export default createClient({
     space: cSpace,
     accessToken: cToken
